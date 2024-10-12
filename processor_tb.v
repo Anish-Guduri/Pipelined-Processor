9e0833e9c910fd7540ef88a0ffafcb9ba55999de
Anish Guduri <gudurianish@gmail.com>
2024-10-13 01:37:06 +0530

    memory is reading from .hex file

--- processor_tb.v
+++ processor_tb.v
@@ -14,21 +14,30 @@ initial begin
     for(k=0;k<32;k=k+1)
        risc_p.RegFile[k]=0;
 
-    risc_p.Memory[0]=32'h2801000a;    // ADD R1, R0, 10
-    risc_p.Memory[1]=32'h28020014;    // ADDi R2, R0, 20
-    risc_p.Memory[2]=32'h00222000;   // ADD R4, R1, R2  
-    risc_p.Memory[3]=32'hfc000000;   // HLT
+    // risc_p.Memory[9]=32'h2801000a;    // ADD R1, R0, 10
+    // risc_p.Memory[10]=32'h28020014;    // ADDi R2, R0, 20
+    // risc_p.Memory[11]=32'h00222000;   // ADD R4, R1, R2  
+    // risc_p.Memory[12]=32'hfc000000;   // HLT
 
     risc_p.HALTED=0;
     risc_p.PC=0;
     risc_p.TAKEN_BRANCH = 0;
 
     #250
-    for(k=0;k<6;k++)
+    for(k=0;k<6;k=k+1)
     $display ("R%1d  - %2d",k, risc_p.RegFile[k]);
-    for(k=0;k<6;k++)
-    $display ("MEM[%1d]  - %2d",k, risc_p.Memory[k]);
+
 end
+
+    // Initialize memory contents
+initial begin
+        // Load initial memory contents from a hex file
+    $readmemh("instructions.hex", risc_p.Memory); // Adjust the range as needed
+
+    #500
+    for(k=0; k < 20; k=k+1)
+    $display ("MEM[%1d]  - %2d",k, risc_p.Memory[k]);
+    end
 
 initial 
     begin
